a6c74ca6d3b9d2f54016bbdd07fa377a4e976e05
Saad-Codes27 <saadsayyed225@gmail.com>
2026-09-24 17:58:05 +0530

    Enhance guessing game with attempt tracking
    Added attempt counter to track number of guesses.

--- day4.py
+++ day4.py
@@ -1,13 +1,16 @@
 import random 
 
 secret = random.randint(1,100)
+attempts=0
+
 print("I have picked a number between 1 and 100!")
 
 while 'true':
       guess = int(input("your guess:"))
+      attempts=attempts + 1
 
       if guess == secret: 
-         print(f"BOOM! You guessed it! the number was {secret}")
+         print(f"YES! You guessed it in {attempts} tries")
          break
       elif guess<secret:
           print("Too low! Try higher")
